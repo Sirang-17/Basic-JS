ce87002c4a0366cfc2f8afa3f471ff96df220eaf
Sirang-17 <sirangmuhammad17@gmail.com>
2026-01-16 22:59:52 +0500

    added new files

---
+++ Functions/class-work/script.js
@@ -0,0 +1,32 @@
+// function name() {
+// }
+
+
+let myFirstFunction = function () {
+    console.log("function here");
+}
+myFirstFunction();
+
+
+function myAge() {
+    console.log("i am 20 years old");
+}
+myAge();
+
+
+
+// Function Parameters and Arguments    
+function myData(name, lName) {
+    console.log(name, lName);
+}
+myData("saima", "akhtar")
+// let name = "saima"    
+// // let lName = "akhtar"  
+
+
+  
+function addTwoNumbers(a, b, c) {
+    return a + b + c
+    console.log(a + b + c);
+}
+console.log(addTwoNumbers(10, 39, 2));
---
+++ Loops/class-work/index.js
@@ -0,0 +1,28 @@
+// initialization ; condition ; incrment/decrement
+
+    for (let index = 0; index < 15; index++) {
+       console.log(index);
+            //  index < 15 => 1 < 15 true
+            //  index < 15 => 2 < 15 true
+            //  index < 15 => 3 < 15 true
+            // .
+            //  index < 15 => 14 < 15 true
+            //  index < 15 => 15 < 15 false
+
+       
+        // initialization ; condition ; incrment/decrement
+    }
+
+// While
+let number = 1
+while (number < 20 ) {
+    console.log(number);
+    number++ 
+}
+let i = 3
+do {
+    console.log(i, "do while loop");
+    i++ 
+    
+    
+} while (i <= 10);
